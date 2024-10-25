ad251ba0b5b74798410f7d3614556eddaab49c52
Kyaw-Thiha <k.thiha10.mail@gmail.com>
2024-10-25 06:36:10 -0400

    Implemented the algorithm

--- src/routes/admin-sign-in.lazy.tsx
+++ src/routes/admin-sign-in.lazy.tsx
@@ -1,10 +1,14 @@
-import { createLazyFileRoute } from '@tanstack/react-router'
+import { getPath } from "@/lib/getPath";
+import { testData } from "@/lib/unorganisedTestData";
+import { createLazyFileRoute } from "@tanstack/react-router";
 
-export const Route = createLazyFileRoute('/admin-sign-in' as never)({
+export const Route = createLazyFileRoute("/admin-sign-in" as never)({
   component: AdminSignIn,
-})
+});
 
 function AdminSignIn() {
-  return <div className="p-2">Hello from About!</div>
-}
+  const paths = getPath(testData, 3);
+  console.log(paths);
 
+  return <div className="p-2">Hello from About!</div>;
+}
